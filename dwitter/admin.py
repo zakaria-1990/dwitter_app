0b923eab7dabe6ae10779a93405dc2dbaecd9597
zakaria <zakaria.monerobit@gmail.com>
2022-09-15 15:32:26 +0100

    linking user creation with user profile

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -3,13 +3,16 @@ from django.contrib.auth.models import Group, User
 from .models import Profile
 
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ['username']
+    inlines = [ProfileInline]
 
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-admin.site.register(Profile)
+#  admin.site.register(Profile)
 admin.site.unregister(Group)
--- dwitter/models.py
+++ dwitter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -11,4 +12,18 @@ class Profile(models.Model):
         symmetrical=False,
         blank=True,
     )
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
+
+#  post_save.connect(create_profile, sender=User)
+
 
